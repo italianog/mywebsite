ef25616f561268ecf2d88a1315332185a22cdceb
Giovanni <58092685+italianog@users.noreply.github.com>
2020-07-08 17:54:02 +0200

    header component and homepage page

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./header.styles.css";
+
+const Header = () => (
+  <div className="header">
+    <div className="container">
+      <div className="logo-container"></div>
+      <div className="options">
+        <div className="option">Chi siamo</div>
+        <div className="option">Contattaci</div>
+        <div className="option">Clienti</div>
+        <div className="option">Iscriviti</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.css";
+import Header from "../../components/header/header.component";
+
+const HomePage = () => (
+  <div className="home-page">
+    <Header />
+  </div>
+);
+
+export default HomePage;
